881b26db8f7616cb0fd05a6034944791c6f4fc53
Kent Edoloverio <69900896+kents00@users.noreply.github.com>
2023-09-03 00:56:14 +0800

    Adding new features

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "blender" : (3,6,1),
     "category" : "3D View",
     "location" : "3D View > Make that Clay",
-    "version" : (1,0,0),
+    "version" : (1,2),
     "author" : "Kent Edoloverio",
     "description" : "Turns mesh into clay",
     "wiki_url" : "",
@@ -24,15 +24,12 @@ class Clay(Operator):
 
     @classmethod
     def poll(cls, context):
-        pass
+        return context.active_object and context.active_object.type == 'MESH'
 
     def import_file(self):
         if not os.path.isfile(self.source_file):
             self.report({'ERROR'}, "File not found: {}".format(self.source_file))
         return {'FINISHED'}
-
-    def create_node_group(self):
-        pass
 
     def import_node_group(self, node_group_name):
         with bpy.data.libraries.load(self.source_file, link=False) as (data_from, data_to):
@@ -42,11 +39,11 @@ class Clay(Operator):
         if not data_to.node_groups or not data_to.node_groups[0]:
             self.report({'ERROR'}, "Failed to load the node group: {}".format(node_group_name))
             self.report({'INFO'}, "Creating new node group: {}".format(node_group_name))
-            self.create_node_group
             return {'FINISHED'}
 
     def execute(self):
-        pass
+        self.import_node_group("Makethatclay")
+        return {'FINISHED'}
 
 class ClayPanel(Panel):
     bl_label = "Make that Clay"
@@ -56,7 +53,26 @@ class ClayPanel(Panel):
     bl_options = {'HEADER_LAYOUT_EXPAND'}
 
     def draw(self,context):
-        pass
+        layout = self.layout
+
+        col = layout.row(align=False)
+        image = bpy.data.images.load(os.path.join(os.path.dirname(__file__), "..", "MakethatClay/data", "logo.png"))
+        col.image(image, use_pixels_as_ico=True)
+
+        col = layout.row(align=False)
+        col.enabled = True
+        col.scale_x = 1.2
+        col.scale_y = 1.2
+        col.operator("material.append_make_that_clay")
+
+        col = layout.column(align=False)
+        col.label(text=r"SUPPORT ME ON:")
+        op = self.layout.operator(
+            'wm.url_open',
+            text='KO-FI',
+            icon='URL'
+            )
+        op.url = 'https://ko-fi.com/kents_workof_art'
 
 classes = (
     Clay,
