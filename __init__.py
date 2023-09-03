50d6412d760398bad75ced6cea09c23d5a1fc78c
Kent Edoloverio <69900896+kents00@users.noreply.github.com>
2023-09-03 16:29:59 +0800

    Adding the material and link to material output

--- __init__.py
+++ __init__.py
@@ -29,6 +29,7 @@ class Clay(Operator):
     def import_file(self):
         if not os.path.isfile(self.source_file):
             self.report({'ERROR'}, "File not found: {}".format(self.source_file))
+            return {'CANCELLED'}
         return {'FINISHED'}
 
     def import_node_group(self, node_group_name):
@@ -39,9 +40,45 @@ class Clay(Operator):
         if not data_to.node_groups or not data_to.node_groups[0]:
             self.report({'ERROR'}, "Failed to load the node group: {}".format(node_group_name))
             self.report({'INFO'}, "Creating new node group: {}".format(node_group_name))
-            return {'FINISHED'}
 
-    def execute(self):
+        material = bpy.data.materials.new(name=node_group_name)
+        material.use_nodes = True
+        if bpy.context.object is not None:
+            bpy.context.object.data.materials.append(material)
+
+        tree = material.node_tree
+        group_node = tree.nodes.new(type='ShaderNodeGroup')
+        group_node.node_tree = data_to.node_groups[0]
+        group_node.location = (-40, 300)
+        group_node.use_custom_color = True
+        group_node.color = (0,0,0)
+        group_node.inputs[0].default_value = (0.00151755, 0.043735, 0.0802198, 1)
+        group_node.inputs[1].default_value = (1, 0.496933, 0.107023, 1)
+        group_node.inputs[2].default_value = 25
+        group_node.inputs[3].default_value = 2.5
+        group_node.inputs[4].default_value = 0.150
+        group_node.inputs[5].default_value = -0.150
+        group_node.inputs[6].default_value = 1
+        group_node.inputs[7].default_value = 15
+        group_node.inputs[8].default_value = -1.2
+        group_node.inputs[9].default_value = 0
+        group_node.width = 250
+
+        principled_bsdf_node = tree.nodes.get('Principled BSDF')
+        if principled_bsdf_node:
+            tree.nodes.remove(principled_bsdf_node)
+
+        shader_node_output_material_node = tree.nodes.get('Material Output')
+        if shader_node_output_material_node:
+            links = tree.links
+            links.new(group_node.outputs[0], shader_node_output_material_node.inputs[0])
+            links.new(group_node.outputs[1], shader_node_output_material_node.inputs[2])
+
+        self.report({'INFO'}, "Successfully appended node group: {}".format(node_group_name))
+        return {'FINISHED'}
+
+    def execute(self,context):
+        self.import_file
         self.import_node_group("Makethatclay")
         return {'FINISHED'}
 
@@ -56,13 +93,9 @@ class ClayPanel(Panel):
         layout = self.layout
 
         col = layout.row(align=False)
-        image = bpy.data.images.load(os.path.join(os.path.dirname(__file__), "..", "MakethatClay/data", "logo.png"))
-        col.image(image, use_pixels_as_ico=True)
-
-        col = layout.row(align=False)
         col.enabled = True
-        col.scale_x = 1.2
-        col.scale_y = 1.2
+        col.scale_x = 2
+        col.scale_y = 2
         col.operator("material.append_make_that_clay")
 
         col = layout.column(align=False)
